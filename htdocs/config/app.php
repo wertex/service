ffb6e13690d6a001fec68635b34155e0a4361b07
wertex <wertex15@gmail.com>
2015-10-31 23:03:13 +0500

    Установил barryvdh/laravel-ide-helper, cгенерировал Установил doctrine/dbal

--- htdocs/config/app.php
+++ htdocs/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'ru',
 
     /*
     |--------------------------------------------------------------------------
@@ -144,6 +144,11 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+		
+		/*
+			wertex
+		*/
+		Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
